f922e6b637a208d206e3bd13bf5647f711010b6d
jingc <jingciheng@gmail.com>
2026-02-22 15:00:11 -0800

    Add Sapphire item textures, models and languages

--- src/main/java/net/jhhhh/tutorialmod/TutorialMod.java
+++ src/main/java/net/jhhhh/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.jhhhh.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.jhhhh.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -40,6 +42,9 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if( event.getTabKey() == CreativeModeTabs.INGREDIENTS ) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/jhhhh/tutorialmod/item/ModItems.java
+++ src/main/java/net/jhhhh/tutorialmod/item/ModItems.java
@@ -7,7 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ModItem {
+public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
