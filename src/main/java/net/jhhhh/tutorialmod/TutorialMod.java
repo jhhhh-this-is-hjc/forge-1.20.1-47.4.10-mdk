24d5c97320f23336b0e2ea8b0239ae6e5d4969ce
jingc <jingciheng@gmail.com>
2026-02-23 17:12:50 -0800

    Setup the basic structure of a block, including some helper methods

--- src/main/java/net/jhhhh/tutorialmod/TutorialMod.java
+++ src/main/java/net/jhhhh/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.jhhhh.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.jhhhh.tutorialmod.block.ModBlocks;
 import net.jhhhh.tutorialmod.item.ModItems;
 import net.jhhhh.tutorialmod.item.ModeCreativeModTabs;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -31,6 +32,7 @@ public class TutorialMod
 
         ModeCreativeModTabs.register(modEventBus);
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/jhhhh/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,38 @@
+package net.jhhhh.tutorialmod.block;
+
+import net.jhhhh.tutorialmod.TutorialMod;
+import net.jhhhh.tutorialmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+
+
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> supplier) {
+        RegistryObject<T> block = BLOCKS.register(name, supplier);
+        registerBlockItem(name, block);
+        return block;
+    }
+
+    /**
+     * Helper method that registers a block item associating with the block
+     */
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
