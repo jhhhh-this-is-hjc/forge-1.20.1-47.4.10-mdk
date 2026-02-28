640e76b1d4fd14568b0fa21b46dc4bf09e509850
jingc <jingciheng@gmail.com>
2026-02-27 21:17:35 -0800

    add sapphire_stairs _fence _fence_gate _wall _button _door _trapdoor _slab _pressure_plate

--- src/main/java/net/jhhhh/tutorialmod/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/jhhhh/tutorialmod/datagen/ModBlockTagGenerator.java
@@ -44,5 +44,19 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                         ModBlocks.NETHER_SAPPHIRE_ORE.get(),
                         ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
                         ModBlocks.SOUND_BLOCK.get());
+
+        this.tag(BlockTags.FENCES)
+                .add(ModBlocks.SAPPHIRE_FENCE.get());
+        this.tag(BlockTags.FENCE_GATES)
+                .add(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+        this.tag(BlockTags.WALLS)
+                .add(ModBlocks.SAPPHIRE_WALL.get());
+
+        this.tag(BlockTags.BUTTONS)
+                .add(ModBlocks.SAPPHIRE_BUTTON.get());
+        this.tag(BlockTags.PRESSURE_PLATES)
+                .add(ModBlocks.SAPPHIRE_PRESSURE_PLATE.get());
+
+
     }
 }
